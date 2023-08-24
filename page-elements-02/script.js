fd06ea80b13bd6cbe5d13f277c4e327172f4715b
Janice Shaw <hello@janiceshaw.co.uk>
2023-08-24 17:03:09 +0200

    added navigation menu

--- page-elements-02/script.js
+++ page-elements-02/script.js
@@ -1,11 +1,36 @@
-console.log('hello')
 const menuButton = document.querySelector('.menu-button');
+const iMask = document.querySelector('h1');
+const navigation = document.querySelector('nav');
+
 console.log(menuButton);
 
-menuButton.addEventListener("click", (element) => {
-    console.log('jkjkj')
-    const button = element.target;
-    button.classList.toggle("open");
-}
-);
+menuButton.addEventListener("mouseover", (element) => {
+    iMask.classList.add('hover')
+});
 
+menuButton.addEventListener("mouseleave", (element) => {
+    iMask.classList.toggle('hover')
+})
+
+menuButton.addEventListener("click", (element) => {
+
+    const button = element.currentTarget;
+    button.classList.toggle("open");
+    navigation.classList.toggle('zoom')
+
+    hamburgerClickHandler(button);
+});
+
+const hamburgerClickHandler = (target) => {
+
+    // console.log('ham', target)
+    const expanded = target.getAttribute('aria-expanded');
+
+    if (expanded === 'false') {
+        target.setAttribute('aria-expanded', 'true');
+
+    } else {
+        target.setAttribute('aria-expanded', false);
+
+    }
+}
